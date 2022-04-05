f4f54a26899e9fad9a83069e6808f9bc598f7622
orozcoding <orozcoding@gmail.com>
2022-04-05 12:01:48 -0400

    Add capitalize and trimmer classes

--- decorator.rb
+++ decorator.rb
@@ -1,11 +1,27 @@
 require "./nameable"
 
+# Decorator class
 class Decorator < Nameable
   def initialize(nameable)
+    super()
     @nameable = nameable
   end
 
   def correct_name
     @nameable.correct_name
+  end
+end
+
+# Capitalize Decorator
+class CapitalizeDecorator < Decorator
+  def correct_name
+    @nameable.correct_name.capitalize
+  end
+end
+
+# Trimmer Decorator
+class TrimmerDecorator < Decorator
+  def correct_name
+    @nameable.correct_name.strip
   end
 end
