29eb2e08e885ed950b1efaf148a2b865f982caaa
orozcoding <orozcoding@gmail.com>
2022-04-07 13:22:50 -0400

    Create rentals methods

--- main.rb
+++ main.rb
@@ -21,22 +21,29 @@ def option_number
 end
 
 def add_person(app)
-  puts 'Do you want to register a (1)-Student  or a (2)-Teacher? [Input the number]'
+  puts 'Do you want to register a [1]-Student or a [2]-Teacher? [Input the number]:'
   type = gets.chomp.to_i
+  return puts 'Please input 1 for Student or 2 for Teacher' unless [1,2].include?(type)
+
   puts 'Name: '
   name = gets.chomp.capitalize
   puts 'Age: '
   age = gets.chomp.to_i
 
+  specialization = ''
+  permission = ''
+
   case type
   when 1
     puts 'Has parent permission? [y/n]: '
     permission = gets.chomp.downcase
-    app.add_person(type, name, age, specialization = '', permission)
+    app.add_person(type, name, age, specialization, permission)
   when 2
     puts 'Specialization: '
     specialization = gets.chomp.capitalize
-    app.add_person(type, name, age, specialization, permission = '')
+    app.add_person(type, name, age, specialization, permission)
+  else
+    return
   end
 
   puts 'Person successfully registered!'
@@ -50,7 +57,49 @@ def add_book(app)
 
   app.add_book(title, author)
   puts 'Book successfully created!'
+end
 
+def add_rental(app)
+  puts 'Select a book from the following list by its number:'
+  i = 1
+  app.books.each do |b|
+    puts "[#{i}] - Title: #{b.title}, Author: #{b.author}"
+    i += 1
+  end
+  book_index = gets.chomp.to_i - 1
+
+  puts 'Select a person from the following list by its number:'
+  i = 1
+  app.people.each do |p|
+    puts "[#{i}] - ID: #{p.id}, Name: #{p.name}, Age: #{p.age}"
+    i += 1
+  end
+  person_index = gets.chomp.to_i - 1
+
+  puts 'Date: '
+  date = gets.chomp
+
+  person = app.people[person_index]
+  book = app.books[book_index]
+
+  app.add_rental(date, person, book)
+
+  puts 'Rental created successfully!'
+end
+
+def display_rentals(app)
+  puts 'Select a person\'s ID from this list'
+  app.people.each do |p|
+    puts "ID: #{p.id}, Name: #{p.name}"
+  end
+
+  puts ''
+  puts 'ID of person: '
+  person_id = gets.chomp.to_i
+  puts ''
+  puts 'Rentals: '
+  app.list_rentals_by_id(person_id)
+  puts ''
 end
 
 def run_option(app, option)
@@ -59,11 +108,12 @@ def run_option(app, option)
   when 2 then app.list_people
   when 3 then add_person(app)
   when 4 then add_book(app)
+  when 5 then add_rental(app)
+  when 6 then display_rentals(app)
   else
     puts 'Please select a valid number from the list!'
   end
 end
-
 
 def main
   app = App.new
